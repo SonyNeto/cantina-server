62e44b54a9416f08c2970cd4b38cc626a2fa7abf
SonyNeto <sonyneto@gmail.com>
2026-07-24 21:06:45 -0300

    chore: run migration to parse old menu items prices to new format

---
+++ migrations/002-parse-menu-items-prices-to-cents.js
@@ -0,0 +1,38 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const MenuItem = require('../models/menuItem');
+
+const mongoose = require('mongoose');
+
+async function run() {
+  await mongoose.connect(process.env.DB_URL);
+
+  const menuItems = await MenuItem.find();
+
+  for (const menuItem of menuItems) {
+    const res = await MenuItem.updateOne(
+      {
+        _id: menuItem._id,
+        label: menuItem.label,
+      },
+      {
+        $set: {price: Math.round(menuItem.price*100)}
+      }
+    );
+
+    if (res.matchedCount === 0) {
+      console.warn(`Item não encontrado: ${menuItem.label}/${menuItem._id}`);
+    }
+  }
+
+  await mongoose.disconnect();
+  console.log('Migration concluída');
+}
+
+run().catch(async (err) => {
+  console.error(err);
+  await mongoose.disconnect();
+  process.exit(1);
+});
