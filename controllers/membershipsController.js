2b358a4d0d46d73bdb0514c16b044a27840d4be1
SonyNeto <sonyneto@gmail.com>
2026-08-19 01:42:03 -0300

    feat: add memberships endpoint
    # Conflicts:
    #	routes/workspace.routes.js

---
+++ controllers/membershipsController.js
@@ -0,0 +1,40 @@
+const Membership = require('../models/membership');
+
+const fetchMemberships = async (req, res) => {
+  const { workspaceId } = req.params;
+	const page = Number(req.query.page);
+  const limit = Number(req.query.limit);
+
+	let membershipsQuery = Membership.find({ workspaceId }).sort({ role: 1 }).populate('userId', 'email');;
+
+	let pagination = null;
+
+	if (page && limit) {
+		membershipsQuery = membershipsQuery.skip((page - 1) * limit).limit(limit);
+
+		const numberOfMenuItems = await Membership.countDocuments({ workspaceId });
+
+		const totalPages = Math.ceil(numberOfMenuItems / limit);
+		const nextPage = page < totalPages ? page + 1 : null;
+
+		pagination = {
+			page,
+			totalPages,
+			nextPage,
+		};
+	}
+
+	const memberships = await membershipsQuery;
+
+	const membershipsWithEmail = memberships.map((membership) => ({
+		...membership.toObject(),
+		email: membership.userId.email,
+		userId: membership.userId._id,
+	}));
+
+	res.json({ memberships: membershipsWithEmail, pagination })
+};
+
+module.exports = {
+	fetchMemberships,
+}
